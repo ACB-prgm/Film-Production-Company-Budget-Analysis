f574be22aee7feac53fe7f74a22d6481a38d78d2
ACB-prgm <aaronbastian31@gmail.com>
2023-06-21 15:39:39 -0700

    testing EB timeout error
    working locally

--- application.py
+++ application.py
@@ -18,9 +18,10 @@ TOKEN_URL = 'https://api.dropboxapi.com/oauth2/token'
 
 @application.route('/')
 def index():
+    return "test"
     # Redirect the user to the Dropbox authorization URL
-    auth_url = f"{AUTHORIZE_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
-    return redirect(auth_url)
+    # auth_url = f"{AUTHORIZE_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
+    # return redirect(auth_url)
 
 @application.route('/auth/callback')
 def auth_callback():
