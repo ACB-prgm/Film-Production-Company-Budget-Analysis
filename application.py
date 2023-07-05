8d7a79cbe59841e4045462fed885ba811f80308f
ACB-prgm <aaronbastian31@gmail.com>
2023-07-04 17:12:46 -0700

    Gspread working

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import Flask, redirect, url_for, request, render_template
-from google_auth_oauthlib.flow import Flow
 from urllib.parse import urlencode
 from modules import DBXReader
 import requests
 import dropbox
+import gspread
 import base64
 import boto3
 import json
@@ -25,7 +25,7 @@ GOOGLE_TOKENS = "google_tokens.json"
 DBX_OAUTH_SECRETS = "dbx_secrets.json"
 DBX_TOKENS = "dbx_tokens.json"
 
-GOOGLE_SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+GOOGLE_SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
 s3 = boto3.client("s3")
 
@@ -35,10 +35,8 @@ s3 = boto3.client("s3")
 def index():
     return render_template("home.html")
 
-
 @application.route("/auth/login", methods=["GET"])
 def login():
-    print("LOGIN")
     # DBX FLOW
     token_valid = dbx_token_valid()
     if os.environ.get("dbx_access_token") and token_valid:
@@ -48,9 +46,9 @@ def login():
         success = refresh_dbx_token()
         if not success:
             print("dbx login again")
-            return redirect(dbx_signin_url())
+            return redirect(dbx_auth_url())
     else:
-        return redirect(dbx_signin_url())
+        return redirect(dbx_auth_url())
         
     # GOOGLE FLOW
     token_valid = google_token_valid()
@@ -63,7 +61,7 @@ def login():
             print("google login again")
             return redirect(google_auth_url())
     else:
-        redirect(google_auth_url())
+        return redirect(google_auth_url())
     
     return render_template("link_submission.html")
 
@@ -94,17 +92,23 @@ def auth_callback(service):
 
 @application.route('/submit', methods=['POST'])
 def submit():
-    link = request.form['link']
+    os.environ["dbx_link"] = request.form['link']
+    return render_template("processing.html", url=url_for("process_data"), message="Your data is being processed. This may take some time, and the page will appear unresponsive.")
     
+@application.route('/processing/datasets', methods=['GET'])
+def process_data():
     dbx = dropbox.Dropbox(os.environ["dbx_access_token"])
-    dbx_reader = DBXReader.DbxDataRetriever(link, dbx)
+    dbx_reader = DBXReader.DbxDataRetriever(os.environ["dbx_link"], dbx)
     
     dbx_reader.create_datasets()
+    
+    upload_dfs_to_google_sheet(dbx_reader.datasets, "626_budget_analysis")
 
-    return "Datasets Created!"
+    return "success"
+    # return render_template("processing.html", url=url_for("upload_to_google_sheets"), message="Your data is being processed. This may take some time, and the page will appear unresponsive.")
 
 # HELPERS ————————————————————————————————————————————————————————————————————————————————————————————————————————
-def dbx_signin_url() -> str:
+def dbx_auth_url() -> str:
     # Redirect the user to the Dropbox authorization URL
     secrets = get_dbx_secrets()
     params = {
@@ -125,7 +129,6 @@ def google_auth_url() -> str:
         "scope" : " ".join(GOOGLE_SCOPES),
         "access_type" : "offline"
     }
-    print(params)
     return f"{gsecrets['auth_uri']}?{urlencode(params)}"
 
 def dbx_token_valid() -> bool:
@@ -144,12 +147,12 @@ def dbx_token_valid() -> bool:
         return False
 
 def refresh_dbx_token() -> bool:
-    if not os.environ.get("refresh_token"):
+    if not os.environ.get("dbx_refresh_token"):
         return False
     secrets = get_dbx_secrets()
     data = {
         'grant_type' : 'refresh_token',
-        'refresh_token' : os.environ["refresh_token"],
+        'refresh_token' : os.environ["dbx_refresh_token"],
     }
     # Prepare the headers
     auth = base64.b64encode(f"{secrets['client_id']}:{secrets['client_secret']}".encode()).decode()
@@ -235,6 +238,43 @@ def populate_environ_tokens() -> None:
         tokens = get_s3_tokens(service)
         for token in ["access_token", "refresh_token"]:
             os.environ["%s_%s" % (service, token)] = tokens[token]
+
+def upload_dfs_to_google_sheet(dfs:dict, sheet_name:str):
+    gc = create_gspread_client()
+
+    try:
+        # Try to open the Google Sheet if it exists
+        sheet = gc.open(sheet_name)
+    except gspread.exceptions.SpreadsheetNotFound:
+        # Create a new Google Sheet if it doesn't exist
+        sheet = gc.create(sheet_name)
+
+
+    for idx, df_name in enumerate(dfs):
+        df = dfs.get(df_name)
+        try:
+            worksheet = sheet.get_worksheet(idx)
+            worksheet.clear()
+            worksheet.update_title(df_name)
+        except gspread.WorksheetNotFound:
+            worksheet = sheet.add_worksheet(df_name, len(df), len(df.columns))
+        
+        worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+    # sheet.share(share_email, "user", "writer", notify=False)
+
+def create_gspread_client():
+    secrets = get_google_secrets()
+
+    authorized_user = {
+        "refresh_token": os.environ.get("google_refresh_token"),
+        "token_uri": secrets["token_uri"],
+        "client_id": secrets["client_id"],
+        "client_secret": secrets["client_secret"],
+    }
+    gc, authorized_user = gspread.oauth_from_dict(authorized_user_info=authorized_user)
+
+    return gc
 
 
 if __name__ == '__main__':
